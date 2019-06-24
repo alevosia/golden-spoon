381bc4f9cef8e72ed9ca3255a0c2cdfe2e7e4e45
alevfalse <exactfalse@gmail.com>
2019-06-24 23:06:04 +0800

    home page

--- public/js/home.js
+++ public/js/home.js
@@ -1,49 +1,54 @@
 $(document).ready(function() {
-    //fitty('#salabat-heading');
+	//fitty('#salabat-heading');
+    $('.main-gallery').flickity({
+        // options
+        contain: true,
+        wrapAround: true,
+	});
+	
+	setInterval(function() {
+		if (!document.hidden) {
+			$('.main-gallery').flickity('next');
+		}
+	}, 2000);
 });
+
+$('#navbar-toggler').click(function() {
+	
+})
 
 // Smooth Scroll
 $('a.nav-link').click(function(e) {
-    if (this.hash) {
-        e.preventDefault();
-        
-        const hash = this.hash;
-        
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800);
-    }
+	if (this.hash) {
+		e.preventDefault();
+
+		const hash = this.hash;
+
+		$('html, body').animate(
+			{
+				scrollTop: $(hash).offset().top
+			},
+			800
+		);
+	}
 });
 
 $('#bestseller-button').click(function(e) {
-    $('html, body').animate({
-        scrollTop: $('#products-section').offset().top + 1
-    }, 800);
-    
-    /*
+	$('html, body').animate(
+		{
+			scrollTop: $('#products-section').offset().top + 1
+		},
+		800
+	);
+
+	/*
     $(this).animate({
         top: '100%'
     }, 800);*/
-    
-    setTimeout(function() {
-        const button = document.getElementById('bestseller-button');
-        button.blur();
-        console.log('Blurred!');
-    }, 800);
+
+	setTimeout(function() {
+		const button = document.getElementById('bestseller-button');
+		button.blur();
+		console.log('Blurred!');
+	}, 800);
 });
-
-/*
-window.onscroll = function() {
-    const navbar = document.getElementById('main-navbar')
-    const scrollTop = document.documentElement ? document.documentElement.scrollTop : document.body.scrollTop;
-
-    console.log(scrollTop);
-    
-    if (scrollTop > navbar.clientHeight) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-}
-*/
-

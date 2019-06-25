022c04e45810e198ba6a570e44755d0c40f4f40e
alevfalse <exactfalse@gmail.com>
2019-06-25 12:00:48 +0800

    removed smooth scroll

--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
-	//fitty('#salabat-heading');
-    $('.main-gallery').flickity({
-        // options
-        contain: true,
-        wrapAround: true,
+	$('.main-gallery').flickity({
+		contain: true,
+		wrapAround: true,
 	});
 	
 	setInterval(function() {
@@ -13,42 +11,13 @@ $(document).ready(function() {
 	}, 2000);
 });
 
-$('#navbar-toggler').click(function() {
-	
-})
-
-// Smooth Scroll
-$('a.nav-link').click(function(e) {
-	if (this.hash) {
-		e.preventDefault();
-
-		const hash = this.hash;
-
-		$('html, body').animate(
-			{
-				scrollTop: $(hash).offset().top
-			},
-			800
-		);
-	}
-});
-
 $('#bestseller-button').click(function(e) {
-	$('html, body').animate(
-		{
-			scrollTop: $('#products-section').offset().top + 1
-		},
-		800
-	);
-
-	/*
-    $(this).animate({
-        top: '100%'
-    }, 800);*/
-
+	$('html, body').animate({
+		scrollTop: $('#products-section').offset().top + 1
+	}, 800);
+	
 	setTimeout(function() {
 		const button = document.getElementById('bestseller-button');
 		button.blur();
-		console.log('Blurred!');
 	}, 800);
 });
